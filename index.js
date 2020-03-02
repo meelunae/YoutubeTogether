a3e9ae6e0ed2b420c8ad68ee5ee268bae52307df
Peachyinu <peachyinu@Emanueles-MacBook-Pro.local>
2020-03-03 00:28:59 +0100

    Added sockets

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var io = require('socket.io').listen(server);
+const path = require('path')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('index'))
+
+  io.on('connection', function(socket) {
+    socket.on('event', function(msg){
+      io.emit('event', msg);
+    });
+    console.log("A user connected!");
+  });
+
